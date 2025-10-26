f2f42a5c92cdbb7e44bef9a28112c629a75a0996
BartoszJar <jarocki24147@stud.akademiabialska.pl>
2025-10-26 20:21:31 +0100

    Zmieniona nazwe aplikacji, zwiekszono przyciski, dodano napis z nazwa aplikacji i zmieniono font na Tahoma

--- lab3/Form1.Designer.cs
+++ lab3/Form1.Designer.cs
@@ -42,13 +42,14 @@
             label3 = new Label();
             label4 = new Label();
             label5 = new Label();
+            label6 = new Label();
             SuspendLayout();
             // 
             // clearBtn
             // 
-            clearBtn.Location = new Point(429, 150);
+            clearBtn.Location = new Point(417, 224);
             clearBtn.Name = "clearBtn";
-            clearBtn.Size = new Size(75, 23);
+            clearBtn.Size = new Size(112, 34);
             clearBtn.TabIndex = 1;
             clearBtn.Text = "Wyczyść";
             clearBtn.UseVisualStyleBackColor = true;
@@ -56,9 +57,9 @@
             // 
             // calcBtn
             // 
-            calcBtn.Location = new Point(348, 151);
+            calcBtn.Location = new Point(290, 224);
             calcBtn.Name = "calcBtn";
-            calcBtn.Size = new Size(75, 23);
+            calcBtn.Size = new Size(112, 34);
             calcBtn.TabIndex = 2;
             calcBtn.Text = "Oblicz";
             calcBtn.UseVisualStyleBackColor = true;
@@ -66,35 +67,35 @@
             // 
             // storageTbox
             // 
-            storageTbox.Location = new Point(185, 49);
+            storageTbox.Location = new Point(201, 99);
             storageTbox.Name = "storageTbox";
             storageTbox.Size = new Size(100, 23);
             storageTbox.TabIndex = 3;
             // 
             // liquidTbox
             // 
-            liquidTbox.Location = new Point(185, 78);
+            liquidTbox.Location = new Point(201, 128);
             liquidTbox.Name = "liquidTbox";
             liquidTbox.Size = new Size(100, 23);
             liquidTbox.TabIndex = 4;
             // 
             // countTbox
             // 
-            countTbox.Location = new Point(185, 107);
+            countTbox.Location = new Point(201, 157);
             countTbox.Name = "countTbox";
             countTbox.Size = new Size(100, 23);
             countTbox.TabIndex = 5;
             // 
             // res1Box
             // 
-            res1Box.Location = new Point(636, 51);
+            res1Box.Location = new Point(664, 101);
             res1Box.Name = "res1Box";
             res1Box.Size = new Size(100, 23);
             res1Box.TabIndex = 6;
             // 
             // res2Box
             // 
-            res2Box.Location = new Point(636, 78);
+            res2Box.Location = new Point(664, 128);
             res2Box.Name = "res2Box";
             res2Box.Size = new Size(100, 23);
             res2Box.TabIndex = 7;
@@ -104,7 +105,7 @@
             // 
             storageCbox.AutoCompleteCustomSource.AddRange(new string[] { "Kieliszek (50ml)", "Szklanka (250ml)", "Butelka (500 ml)", "Dzbanek (800ml)" });
             storageCbox.FormattingEnabled = true;
-            storageCbox.Location = new Point(302, 49);
+            storageCbox.Location = new Point(318, 99);
             storageCbox.Name = "storageCbox";
             storageCbox.Size = new Size(121, 23);
             storageCbox.TabIndex = 8;
@@ -112,7 +113,7 @@
             // liquidCbox
             // 
             liquidCbox.FormattingEnabled = true;
-            liquidCbox.Location = new Point(302, 78);
+            liquidCbox.Location = new Point(318, 128);
             liquidCbox.Name = "liquidCbox";
             liquidCbox.Size = new Size(121, 23);
             liquidCbox.TabIndex = 9;
@@ -120,53 +121,69 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(6, 54);
+            label1.Font = new Font("Tahoma", 9F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            label1.Location = new Point(15, 104);
             label1.Name = "label1";
-            label1.Size = new Size(173, 15);
+            label1.Size = new Size(179, 14);
             label1.TabIndex = 10;
-            label1.Text = "Podaj pojemność naczynia (ml)";
+            label1.Text = "Podaj pojemność naczynia (ml):";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(6, 81);
+            label2.Font = new Font("Tahoma", 9F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            label2.Location = new Point(12, 132);
             label2.Name = "label2";
-            label2.Size = new Size(169, 15);
+            label2.Size = new Size(182, 14);
             label2.TabIndex = 11;
-            label2.Text = "Podaj zawartość substancji (%)";
+            label2.Text = "Podaj zawartość substancji (%):";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(6, 110);
+            label3.Font = new Font("Tahoma", 9F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            label3.Location = new Point(38, 160);
             label3.Name = "label3";
-            label3.Size = new Size(148, 15);
+            label3.Size = new Size(156, 14);
             label3.TabIndex = 12;
-            label3.Text = "Podaj liczbę naczyń (sztuk)";
+            label3.Text = "Podaj liczbę naczyń (sztuk):";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(536, 54);
+            label4.Font = new Font("Tahoma", 9F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            label4.Location = new Point(521, 104);
             label4.Name = "label4";
-            label4.Size = new Size(94, 15);
+            label4.Size = new Size(137, 14);
             label4.TabIndex = 13;
-            label4.Text = "Łączna objętość:";
+            label4.Text = "Łączna objętość (w ml):";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(479, 81);
+            label5.Font = new Font("Tahoma", 9F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            label5.Location = new Point(461, 132);
             label5.Name = "label5";
-            label5.Size = new Size(151, 15);
+            label5.Size = new Size(197, 14);
             label5.TabIndex = 14;
-            label5.Text = "Objętość czystej substancji:";
+            label5.Text = "Objętość czystej substancji (w ml):";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Constantia", 27.75F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label6.Location = new Point(86, 9);
+            label6.Name = "label6";
+            label6.Size = new Size(603, 45);
+            label6.TabIndex = 15;
+            label6.Text = "《 KAKLULATOR PROCENTOWY 》";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(781, 292);
+            Controls.Add(label6);
             Controls.Add(label5);
             Controls.Add(label4);
             Controls.Add(label3);
@@ -182,7 +199,7 @@
             Controls.Add(calcBtn);
             Controls.Add(clearBtn);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Kalulator procentowy";
             ResumeLayout(false);
             PerformLayout();
         }
@@ -203,5 +220,6 @@
         private Label label3;
         private Label label4;
         private Label label5;
+        private Label label6;
     }
 }
